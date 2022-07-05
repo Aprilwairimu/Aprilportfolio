20e4ecac5b0a403068666e62c1bf10c78cb1a27d
Aprilwairimu <april.wairimu@student.moringaschool.comgit config --global user.email april.wairimu@student.moringaschool.comgit config --global user.email april.wairimu@student.moringaschool.com>
2022-07-05 12:21:36 +0300

    added a static route

--- portapp/urls.py
+++ portapp/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('',views.home, name='home'),
@@ -7,4 +9,4 @@ urlpatterns = [
     path('portfolio',views.portfolio, name='portfolio'),
     path('services',views.services, name='services'),
     path('contact',views.contact, name='contact'),
-]
+]+ static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
